b363333ec4cb720d37fb9c5986979b44b945baad
Masum Ahmed <masumahmed1798@gmail.com>
2022-03-25 04:48:30 -0400

    gitignore and comments

--- SupportFunc.cpp
+++ SupportFunc.cpp
@@ -22,38 +22,32 @@ std::string DecimaltoBinary(int num)
 
 std::string DecToHex(int n)
 {
-    // char array to store hexadecimal number
-    char hexaDeciNum[100];
+    char hex[100];
  
-    // counter for hexadecimal number array
     int i = 0;
     while (n != 0) {
-        // temporary variable to store remainder
         int temp = 0;
         temp = n % 16;
          if (temp < 10) {
-            hexaDeciNum[i] = temp + 48;
+            hex[i] = temp + 48;
             i++;
         }
         else {
-            hexaDeciNum[i] = temp + 55;
+            hex[i] = temp + 55;
             i++;
         }
  
         n = n / 16;
     }
  
-    // printing hexadecimal number array in reverse order
     std::string result = "";
     for (int j = i - 1; j >= 0; j--)
-        result += hexaDeciNum[j];
+        result += hex[j];
 
     std::string ResultWithLeadingZeros = "0x";
-    // add try blcok for when result lenght is greater than 8
-    for(int i=0; i<(8 - result.length()); i++)
-    {
+    for(int i=0; i<(8 - result.length()); i++) 
         ResultWithLeadingZeros += "0";
-    }
+    
     ResultWithLeadingZeros += result;
 
     return ResultWithLeadingZeros;
