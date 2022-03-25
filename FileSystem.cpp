7a1203e8bad28091e9cbb89b648c2e51d927db4a
Masum Ahmed <masumahmed1798@gmail.com>
2022-03-25 14:32:06 -0400

    FATtoMem

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include "SupportFunc.cpp"
 
@@ -11,6 +12,7 @@ class FileSystem
 {
 private:
     int BlockSize = 256; // Default Block size is 256 Characters
+    int FATSize = 50;    // in units of blocks
 
 public:
     void CreateVolumeLinkedAllocation(const char *fname = "volume.txt", int bits = 0, int bytes = 0, int kilobytes = 0, int megabytes = 0) const
@@ -28,11 +30,11 @@ public:
         int j = 1;
         for (int i = 0; i < size; i++)
         {
-            if (i % (BlockSize - 8) == 0 && i != 0)  
+            if (i % (BlockSize - 8) == 0 && i != 0)
             {
                 text += DecToHex(j);
                 j++;
-                i+=7; // account for the length of j with the leading zeroes
+                i += 7; // account for the length of j with the leading zeroes
                 continue;
             }
             text += '0';
@@ -63,5 +65,41 @@ public:
         file.write(text.c_str(), text.size());
         file.close();
         print("Done");
+    }
+
+    std::vector<std::vector<std::string>> FATtoMemory() const
+    {
+        std::vector<std::vector<std::string>> FAT;
+        std::vector<std::string> buffer;
+
+        std::fstream file;
+        file.open("volume.txt", std::ios::in);
+        std::string line;
+        if (file.is_open())
+        {
+            getline(file, line);
+            line.substr(0, this->FATSize * this->BlockSize);
+        }
+
+        std::string temp = "";
+        for (auto &i : line)
+        {
+            if (i == ',')
+            {
+                buffer.push_back(temp);
+                temp = "";
+                continue;
+            }
+            else if (i == '|')
+            {
+                buffer.push_back(temp);
+                temp = "";
+                FAT.push_back(buffer);
+                buffer.clear();
+                continue;
+            }
+            temp += i;
+        }
+        return FAT;
     }
 };
--- main.cpp
+++ main.cpp
@@ -6,7 +6,17 @@
 int main()
 {
     FileSystem fs;
-    fs.CreateVolumeLinkedAllocation("volume.txt", 0, 0, 0, 10); // Create a 25 MB virtual file
-    fs.FAT();
-}
+    // fs.CreateVolumeLinkedAllocation("volume.txt", 0, 0, 0, 10); // Create a 25 MB virtual file
+    // fs.FAT();
 
+    std::vector<std::vector<std::string>> FAT = fs.FATtoMemory();
+
+    for (auto &i : FAT)
+    {
+        for (auto &j : i)
+        {
+            std::cout << j << " ";
+        }
+        std::cout << std::endl;
+    }
+}
